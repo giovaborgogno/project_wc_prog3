b4192ed2cd581bd9ce84ba4e1f0295e42c4f23eb
giovaborgogno <giovaborgogno@gmail.com>
2022-10-17 18:52:18 -0300

    basicas, palabras, ocurrencias

---
+++ cmake-build-debug/CMakeFiles/wce.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wce.dir/main.cpp.obj"
+  "CMakeFiles/wce.dir/main.cpp.obj.d"
+  "libwce.dll.a"
+  "wce.exe"
+  "wce.exe.manifest"
+  "wce.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wce.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
